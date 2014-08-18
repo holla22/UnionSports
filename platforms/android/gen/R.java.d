738001094c4215f947208fb20f42a022e9e00bda
Morne Zeelie <morne.zeelie@double-eye.com>
2014-08-18 22:15:51 +0200

    Extra GCM 2

--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Morne\Documents\GitHub\union\platforms\android\gen\com\unionsports\union\R.java \
- : C:\Users\Morne\Documents\GitHub\union\platforms\android\res\drawable\icon.png \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\res\drawable-hdpi\icon.png \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\res\drawable-ldpi\icon.png \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\res\drawable-mdpi\icon.png \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\res\drawable-xhdpi\icon.png \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\res\values\strings.xml \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\res\xml\config.xml \
-C:\Users\Morne\Documents\GitHub\union\platforms\android\bin\AndroidManifest.xml \
